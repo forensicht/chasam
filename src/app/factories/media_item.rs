8cde613ac22af42d6cd0efdfd18d883a7e029832
Tiago Martins <tiago.tsmweb@gmail.com>
2024-05-03 19:50:07 -0300

    Implementation of zoom and grid item selection features.

--- src/app/factories/media_item.rs
+++ src/app/factories/media_item.rs
@@ -1,39 +1,66 @@
 use std::collections::VecDeque;
 
-use crate::app::components::csam::toolbar::{ToolbarInput, SELECT_BROKER};
-use crate::app::models;
-
 use relm4::{
     binding::{Binding, BoolBinding, I32Binding},
+    gtk::glib,
     gtk::{
         self,
         gdk_pixbuf::Pixbuf,
+        glib::object::ObjectExt,
         pango,
-        prelude::{CheckButtonExt, OrientableExt, WidgetExt},
+        prelude::{OrientableExt, WidgetExt},
     },
     typed_view::grid::RelmGridItem,
     RelmWidgetExt,
 };
+
+use crate::app::components::csam::toolbar::{ToolbarInput, SELECT_BROKER};
+use crate::app::models;
 
 #[derive(Debug)]
 pub struct MediaItem {
     pub media: models::Media,
     pub active: BoolBinding,
     pub thumbnail_size: I32Binding,
+    bindings: Vec<glib::Binding>,
 }
 
 impl MediaItem {
     pub fn new(media: models::Media) -> Self {
+        let active = BoolBinding::new(false);
+        active.connect_notify(None, |value, _| {
+            let is_active = value.value();
+            SELECT_BROKER.send(ToolbarInput::SelectedItem(is_active));
+        });
+
         Self {
             media,
-            active: BoolBinding::new(false),
+            active,
             thumbnail_size: I32Binding::new(models::media::THUMBNAIL_SIZE),
+            bindings: vec![],
         }
     }
 
     pub fn set_active(&mut self, is_active: bool) {
         self.media.is_selected = is_active;
         *self.active.guard() = is_active;
+    }
+
+    pub fn is_video(&self) -> bool {
+        match self.media.media_type {
+            models::media::MediaType::Video => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_csam(&self) -> bool {
+        !self.media.match_type.is_empty()
+    }
+
+    fn get_pixbuf(data: &[u8]) -> Option<Pixbuf> {
+        let bytes: VecDeque<u8> = data.into_iter().cloned().collect();
+        let pixbuf = Pixbuf::from_read(bytes).ok()?;
+        Some(pixbuf)
     }
 }
 
@@ -43,11 +70,11 @@ pub struct Widgets {
     label: gtk::Label,
 }
 
-impl Drop for Widgets {
-    fn drop(&mut self) {
-        dbg!(self.label.label());
-    }
-}
+// impl Drop for Widgets {
+//     fn drop(&mut self) {
+//         dbg!(self.label.label());
+//     }
+// }
 
 impl RelmGridItem for MediaItem {
     type Root = gtk::Box;
@@ -77,10 +104,6 @@ impl RelmGridItem for MediaItem {
                         set_halign: gtk::Align::Start,
                         set_valign: gtk::Align::Start,
                         set_css_classes: &["border-spacing"],
-
-                        connect_toggled => move |checkbox| {
-                            SELECT_BROKER.send(ToolbarInput::SelectedItem(checkbox.is_active()));
-                        },
                     },
 
                     #[name(label)]
@@ -112,9 +135,29 @@ impl RelmGridItem for MediaItem {
         } = widgets;
 
         root.set_tooltip(self.media.name.as_str());
-        // picture.add_write_only_binding(&self.thumbnail_size, "height-request");
-        // picture.add_write_only_binding(&self.thumbnail_size, "width-request");
-        // checkbox.add_binding(&self.active, "active");
+
+        let binding = self
+            .thumbnail_size
+            .bind_property(I32Binding::property_name(), picture, "height-request")
+            .sync_create()
+            .build();
+        self.bindings.push(binding);
+
+        let binding = self
+            .thumbnail_size
+            .bind_property(I32Binding::property_name(), picture, "width-request")
+            .sync_create()
+            .build();
+        self.bindings.push(binding);
+
+        let binding = self
+            .active
+            .bind_property(BoolBinding::property_name(), checkbox, "active")
+            .bidirectional()
+            .sync_create()
+            .build();
+        self.bindings.push(binding);
+
         label.set_label(self.media.name.as_str());
 
         if let Some(data) = self.media.data.as_ref() {
@@ -125,31 +168,13 @@ impl RelmGridItem for MediaItem {
         }
 
         root.set_class_active("media-highlight", self.media.is_csam())
-
-        // *self.active.guard() = self.media.is_selected;
     }
 
     fn unbind(&mut self, widgets: &mut Self::Widgets, _root: &mut Self::Root) {
-        // *self.active.guard() = self.media.is_selected;
         widgets.picture.set_pixbuf(None);
-    }
-}
 
-impl MediaItem {
-    fn get_pixbuf(data: &[u8]) -> Option<Pixbuf> {
-        let bytes: VecDeque<u8> = data.into_iter().cloned().collect();
-        let pixbuf = Pixbuf::from_read(bytes).ok()?;
-        Some(pixbuf)
-    }
-
-    pub fn is_video(&self) -> bool {
-        match self.media.media_type {
-            models::media::MediaType::Video => true,
-            _ => false,
+        for binding in self.bindings.drain(..) {
+            binding.unbind();
         }
-    }
-
-    pub fn is_csam(&self) -> bool {
-        !self.media.match_type.is_empty()
     }
 }
